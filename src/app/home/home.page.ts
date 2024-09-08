9a86a99c7bd18f964e56a9b1542679efc030a493
Francheskacisterna <166559994+Francheskacisterna@users.noreply.github.com>
2024-09-08 08:56:08 -0300

    Se añade home más page.html

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  nombreUsuario: string = '';
+
+
+}
