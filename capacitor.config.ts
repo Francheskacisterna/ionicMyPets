f653f518c67655e43d1310c33020f397ea96d3d3
Francheskacisterna <166559994+Francheskacisterna@users.noreply.github.com>
2024-12-02 05:21:17 -0300

    Agrega detalles en vista y apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.pawsy',
   appName: 'Pawsy',
   webDir: 'www',
   android: {
